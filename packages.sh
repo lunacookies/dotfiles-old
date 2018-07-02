bcc34f7d30c34a7047e1297e5a6060bd2e59c137
Aramis Razzaghipour <aramisnoah@gmail.com>
2018-07-02 19:12:09 +1000

    pack: Fix vim-vint/libyaml compat and update all
    Basically, pyyaml bugs out when you install it if libyaml is installed.
    This commit removes libyaml before installing pyyaml, and adds it back
    again after it is done. This commit also adds sections to this growing
    script, and also makes sure to update pip and ruby packages (this gets
    a little tricky with the whole libyaml thing).

--- packages.sh
+++ packages.sh
@@ -3,19 +3,60 @@
 # Get me into the dotfiles folder
 cd ~/dotfiles
 
+
+##
+## Update
+##
+
+# Update brew itself
+brew update
 # Update brew packages
 brew upgrade
-brew update
 
-# Install all packages
+# Update ruby packages (this includes bundler itself)
+bundler update
+
+# We now remove libyaml (a ruby dependency) temporarily because it conflicts
+# with PyYAML (required by vim-vint) during install. We install it again after
+# vim-vint has been installed.
+brew uninstall --ignore-dependencies libyaml
+# Update pip (and related software)
+pip3 install --upgrade setuptools wheel pip
+# Update all pip packages
+pip3 freeze --local > tmp
+pip3 install --user --install-option="--prefix=" -r tmp --upgrade
+rm tmp
+
+
+##
+## Install
+##
+
+# Install all brew and ruby packages
 ./brew.sh
 bundle install
-pip3 install --user --install-option="--prefix=" -r Pipfile
 
-# Clean up if necessary
+# Uninstall libyaml in case it was just installed (from brew.sh)
+brew uninstall --ignore-dependencies libyaml
+# Install all pip packages
+pip3 install --user --install-option="--prefix=" -r Pipfile
+# Reinstate libyaml from before
+brew install libyaml
+
+
+##
+## Clean up
+##
+
+# Clean up as necessary
 brew cleanup
 brew prune
 brew doctor
+
+
+##
+## Install Fira Mono with Text Figures
+##
 
 # Manually download Fira Mono with Text Figures
 git clone https://github.com/aramisgithub/FiraMono_Text_Figures.git ~/fonttmp
