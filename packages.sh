65d9ba02970f24cfb713939f077d35e0459dff2d
Aramis Razzaghipour <aramisnoah@gmail.com>
2018-08-23 20:01:52 +1000

    pack: Simplify installation and don't install gems

--- packages.sh
+++ packages.sh
@@ -3,106 +3,29 @@
 # Get me into the dotfiles folder
 cd ~/dotfiles
 
+# Update
+brew update                 # Update brew itself
+brew upgrade                # Update brew packages
 
-##
-## What to install
-##
-
-brewtaps=(
-caskroom/fonts
-crisidev/homebrew-chunkwm
-koekeishiya/formulae
-)
-
-brewformulae=(
-bash
-chunkwm
-ctags
-exa
-fd
-git
-khd
-neovim
-pandoc
-ranger
-rg
-rmtrash
-shellcheck
-stow
-the_silver_searcher
-wget
-zsh
-)
-
-brewcasks=(
-adobe-acrobat-reader
-alfred
-android-file-transfer
-appcleaner
-flux
-font-fira-sans
-font-m-plus
-google-backup-and-sync
-google-chrome
-liteicon
-mactex
-macvim
-mpv
-soundflower
-soundflowerbed
-)
-
-rubygems=(
-neovim
-)
-
-
-
-##
-## Update
-##
-
-brew update  # Update brew itself
-brew upgrade # Update brew packages
-gem update   # Update gems
-
-
-##
-## Install
-##
-
-# Function to run a command on every item in an array
-installinarray() {
-  array=$1[@]
-  for item in ${!array}; do
-    $2 $item
-  done
-}
-
-installinarray 'brewformulae' 'brew install'   # Formulae
-installinarray 'brewtaps' 'brew tap'           # Taps
-installinarray 'brewcasks' 'brew cask install' # Casks
-installinarray 'rubygems' 'gem install'        # Gems
-
-
-##
-## Clean up
-##
+# Install and delete
+brew bundle cleanup --force # Remove anything not present in the Brewfile
+brew bundle install         # Install everything from the Brewfile
 
 # Clean up as necessary
-brew cleanup
-brew cask cleanup
-brew prune
-brew doctor
+brew cleanup                # Delete caches
+brew cask cleanup           # Delete cask caches
+brew prune                  # Remove unused symlinks
+brew doctor                 # Check if anything is broken
 
 
 ##
 ## Install Fira Mono with Text Figures
 ##
 
+fonturl=https://github.com/aramisgithub/FiraMono_Text_Figures.git
+
 # Download the fonts
-git clone --depth=1 -q https://github.com/aramisgithub/FiraMono_Text_Figures.git ~/fonttmp
-rm -rf ~/fonttmp/.git
+git clone --depth=1 -q $fonturl ~/fonttmp
 
 # Copy them to the user's font directory
 cp ~/fonttmp/*ttf ~/Library/Fonts/
