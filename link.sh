c6a071fe9ca5b84fb837e3dfb895670f85f04376
Aramis Razzaghipour <aramisnoah@gmail.com>
2018-10-19 21:15:48 +1100

    dots: Restructure

--- link.sh
+++ link.sh
@@ -4,14 +4,47 @@
 mkdir -p $HOME/.config
 
 # Get into the dotfiles repository
-cd ~/dotfiles/files
+cd ~/dotfiles
 
-for file in *; do
+# Define a function that links a given file to a given location and
+# automatically removes the target file if it exists
+rmandlink() {
+  local rcpath="$2$1"
+
   # Delete old links if they exist
-  if [ -e $HOME/.$file ]; then
-    rm -rf $HOME/.$file
+  if [ -e $rcpath ]; then
+    rm -rf $rcpath
   fi
 
-  # Link the file
-  ln -s $PWD/$file $HOME/.$file
+  # Link the rc
+  ln -s $PWD/$1 $rcpath
+}
+
+#
+# files
+#
+
+cd files
+
+# Link every file in the files/ directory to $HOME, apart from the ones that
+# follow the XDG Base Directory Spec -- link those to $HOME/.config
+for rc in *; do
+  if [ $rc = "Apple_Terminal" ] \
+    || [ $rc = "git" ] \
+    || [ $rc = "iTerm2" ] \
+    || [ $rc = "nvim" ] \
+    || [ $rc = "stylus" ]; then
+    rmandlink $rc "$HOME/.config/"
+  else
+    rmandlink $rc "$HOME/."
+  fi
 done
+
+cd ..
+
+#
+# bin
+#
+
+# Link my scripts to $HOME
+rmandlink "bin" "$HOME/"
